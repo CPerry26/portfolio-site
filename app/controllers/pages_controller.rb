8c0805d926e9c005aba36774f890f57fe7317219
Cody <cp5337@rit.edu>
2015-09-28 16:49:38 -0400

    Original commit for contact, blog, and portfolio pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,15 @@
            
          end
          
+         def about
+           
+         end
          
+         def portfolio
+           
+         end
+         
+         def contact
+         end
          
        end
